1f616e6067bd83bf8ac9a64b0f91bca4569a9f32
ReMatrixed <mat.vasenin@yandex.ru>
2024-11-30 18:07:03 +0300

    Add empty admin.py, common.py, member.py and user.py

---
+++ src/core/chat/member.py
@@ -0,0 +1,8 @@
+# Подключение модулей библиотеки aiogram
+from aiogram import types, F
+from aiogram import Router, Bot
+from aiogram.filters import StateFilter, Command
+from aiogram.fsm.context import FSMContext
+
+# Подключение core-библиотек
+from core.db.manager import DatabaseManager
