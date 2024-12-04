3fe4aa2ee5925640d563456820167ea1a8c3c846
ReMatrixed <mat.vasenin@yandex.ru>
2024-12-04 22:12:28 +0300

    Change configuration files to enviroment variables in config.py

--- src/core/manager/config.py
+++ src/core/manager/config.py
@@ -1,6 +1,6 @@
-# Подключение библиотеки для работы с JSON-файлами
-import json
-from json.decoder import JSONDecodeError
+# Подключение библиотек для работы с переменными среды
+from dotenv import load_dotenv
+import os
 
 # Подключение библиотеки для работы с аннотациями типов
 from typing import Union
@@ -12,25 +12,15 @@ import logging
 import sys
 
 class ConfigManager:
-    config: dict[str, Union[str, int]] # Словарь доступных параметров
     logger: logging.Logger # Обработчик журнала (логгер)
 
     # Инициализация обработчика, чтение файла конфигурации
     def setup(self, filepath: str) -> None:
         self.logger = logging.getLogger("manager/config.py")
         self.logger.info("Подготовка обработчика конфигураций...")
-        self.logger.info(f"Чтение файла конфигурации: {filepath}...")
-        try:
-            with open(filepath, "r") as config_file:
-                self.config = json.load(config_file)
-        # Так как файл конфигурации критически важен,
-        # программа завершает работу при возникновении ошибки чтения данного файла
-        except FileNotFoundError:
-            self.logger.critical("Ошибка чтения файла конфигурации: Файл не найден")
-            self.logger.info("Завершение работы...")
-            sys.exit() # Безопасное завершение работы программы
-        except JSONDecodeError as e:
-            self.logger.critical(f"Ошибка JSON-парсинга файла конфигурации: ({e})")
+        self.logger.info(f"Чтение файла переменных среды: {filepath}...")
+        if(load_dotenv(filepath) == False):
+            self.logger.critical(f"Ошибка чтения файла {filepath}: Файл не найден")
             self.logger.info("Завершение работы...")
             sys.exit() # Безопасное завершение работы программы
         self.logger.info("Обработчик конфигураций инициализирован.")
@@ -38,7 +28,11 @@ class ConfigManager:
     # Получить значение из конфигурационных данных по ключу
     def get(self, key: str) -> Union[str, int]:
         try:
-            return self.config[key]
+            value = os.environ.get(key) # Получение значения из переменных среды
+            if(value == None): # Если значение не найдено, вызывается исключение
+                raise KeyError
+            else: # Если значение найдено, то оно будет возвращено
+                return value
         except KeyError:
             self.logger.critical(f"Ошибка получения конфигурационных данных: Ключ {key} не найден")
             self.logger.info("Завершение работы...")
